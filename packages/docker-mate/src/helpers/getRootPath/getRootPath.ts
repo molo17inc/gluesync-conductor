e05276f386baa51c16f612d8ea6c087692627562
Daniele Chinazzi <daniele.chinazzi@molo17.com>
2025-07-14 16:00:00 +0200

    fix: getRoot and update LOGGER_LEVEL value

--- packages/docker-mate/src/helpers/getRootPath/getRootPath.ts
+++ packages/docker-mate/src/helpers/getRootPath/getRootPath.ts
@@ -3,6 +3,6 @@ import { join } from 'path';
 import { GetRootPath } from './getRootPath.model';
 
 const getRootPath: GetRootPath = (filename = '') =>
-  join(process.env.PROJECT_CWD as string, filename);
+  join(process.env.PROJECT_CWD || '', filename);
 
 export default getRootPath;
