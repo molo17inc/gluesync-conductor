14300f8200dcfe51bc28ba2707daf1332c9d115e
Alessandro Caregnato <alessandro.caregnato@molo17.com>
2025-09-18 16:18:09 +0200

    removed env var ALLOWED_ORIGINS not used

--- typings.d.ts
+++ typings.d.ts
@@ -16,7 +16,6 @@ declare namespace NodeJS {
     readonly CORE_HUB_NAME: string;
     readonly BASE_PATH: string;
 
-    readonly ALLOWED_ORIGINS: string;
     readonly USE_SDK: string;
     readonly SSL_SKIP_VERIFY: string;
     readonly SSL_ENABLED: string;
