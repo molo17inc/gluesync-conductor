e9d5b23fa218c4fb611c903c41b6e5e9248ddce3
Alessandro Caregnato <alessandro.caregnato@molo17.com>
2025-09-18 10:47:58 +0200

    allowed all origin

--- packages/gluesync-conductor/src/index.ts
+++ packages/gluesync-conductor/src/index.ts
@@ -39,18 +39,7 @@ const startServer = async () => {
 
   // ✅ Register CORS before routes
   await server.register(cors, {
-    origin: (
-      origin: Readonly<string>,
-      cb: (err: Readonly<Error | null>, allow?: Readonly<boolean>) => void,
-    ) => {
-      const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') ?? [];
-
-      if (!origin) return cb(null, true); // allow non-browser clients
-      if (allowedOrigins.includes(origin)) {
-        return cb(null, true);
-      }
-      return cb(new Error('Not allowed by CORS'), false);
-    },
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
