ce050d941e86d38372fd96ea0c503c1f3be2aff5
Alessandro Caregnato <alessandro.caregnato@molo17.com>
2025-12-19 12:10:20 +0100

    used env var for windows image and added network to helper cotainer

--- packages/gluesync-conductor/src/index.ts
+++ packages/gluesync-conductor/src/index.ts
@@ -131,7 +131,10 @@ const startServer = async () => {
   );
 
   const windowsVersion = process.env.WINDOWS_VERSION || '2019';
-  const helperImageWindows = `molo17/docker-helper:28.0.0-win-nanoserver-ltsc${windowsVersion}-develop`;
+  const helperImageBase =
+    process.env.HELPER_IMAGE_BASE ||
+    'molo17/docker-helper:28.0.0-win-nanoserver-ltsc';
+  const helperImageWindows = `${helperImageBase}${windowsVersion}-develop`;
 
   if (rebootNeeded) {
     const isWindows = process.env.IS_WINDOWS?.toLowerCase() === 'true' || false;
