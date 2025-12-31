a2075c57e7ab9985bb17cc23e86828dcccb858e8
Daniele Angeli <daniele.angeli@molo17.com>
2025-12-31 16:53:39 +0100

    typo: use agent_tag

--- packages/gluesync-conductor/src/helpers/composeFile/createComposeService/createComposeService.ts
+++ packages/gluesync-conductor/src/helpers/composeFile/createComposeService/createComposeService.ts
@@ -170,7 +170,7 @@ const createComposeService: CreateComposeService = (
     environment: Object.entries({
       ...environment,
       ...(agentType && { TYPE: agentType }),
-      GLUESYNC_MODULE_TAG: nickname || serviceId,
+      AGENT_TAG: nickname || serviceId,
       ...(serviceType === 'agent' && {
         INITIAL_AGENT_ID: id,
         LOG_CONFIG_FILE: isWindows
