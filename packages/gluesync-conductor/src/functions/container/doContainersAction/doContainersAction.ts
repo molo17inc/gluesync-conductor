cf892f4e5173d3b399790c06c4c430501869ea06
Alessandro Caregnato <alessandro.caregnato@molo17.com>
2026-01-16 09:05:05 +0100

    used stable name for docker image for windows

--- packages/gluesync-conductor/src/functions/container/doContainersAction/doContainersAction.ts
+++ packages/gluesync-conductor/src/functions/container/doContainersAction/doContainersAction.ts
@@ -115,8 +115,8 @@ const handler: DoContainersActionHandler = async (req, reply) => {
         const windowsVersion = process.env.WINDOWS_VERSION || '2019';
         const helperImageBase =
           process.env.HELPER_IMAGE_BASE ||
-          'molo17/docker-helper:28.0.0-win-nanoserver-ltsc';
-        const helperImageWindows = `${helperImageBase}${windowsVersion}-develop`;
+          'molo17/docker:28.0.0-win-nanoserver-ltsc';
+        const helperImageWindows = `${helperImageBase}${windowsVersion}`;
 
         reply.code(200);
         reply.send({
@@ -206,8 +206,8 @@ const handler: DoContainersActionHandler = async (req, reply) => {
           const windowsVersion = process.env.WINDOWS_VERSION || '2019';
           const helperImageBase =
             process.env.HELPER_IMAGE_BASE ||
-            'molo17/docker-helper:28.0.0-win-nanoserver-ltsc';
-          const helperImageWindows = `${helperImageBase}${windowsVersion}-develop`;
+            'molo17/docker:28.0.0-win-nanoserver-ltsc';
+          const helperImageWindows = `${helperImageBase}${windowsVersion}`;
 
           reply.code(200);
           reply.send({
