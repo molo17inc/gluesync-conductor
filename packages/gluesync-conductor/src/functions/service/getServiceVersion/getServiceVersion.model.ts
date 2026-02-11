b695d1619bc3fdfe0d2bafcc58ed59187559f80d
Alessandro Caregnato <alessandro.caregnato@molo17.com>
2026-02-11 15:11:08 +0100

    returned services name when they cause a mandatory update

--- packages/gluesync-conductor/src/functions/service/getServiceVersion/getServiceVersion.model.ts
+++ packages/gluesync-conductor/src/functions/service/getServiceVersion/getServiceVersion.model.ts
@@ -8,6 +8,7 @@ export type GetServiceVersionSuccessResponse = Readonly<{
   latestVersionBeta?: Readonly<string>;
   latestVersionGA?: Readonly<string>;
   mandatoryUpdate: boolean;
+  internalNames: ReadonlyArray<string>;
 }>;
 
 export type GetServiceVersionParams = Readonly<{
