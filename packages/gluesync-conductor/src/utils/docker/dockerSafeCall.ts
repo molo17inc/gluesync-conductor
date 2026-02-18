34ee5e8320439aba1753e1368d1d2433ea704080
Alessandro Caregnato <alessandro.caregnato@molo17.com>
2026-02-18 12:48:01 +0100

    retry docker client

---
+++ packages/gluesync-conductor/src/utils/docker/dockerSafeCall.ts
@@ -0,0 +1,33 @@
+import { getDocker, rebuildDocker } from './getDocker';
+import { getLogger } from '../logger';
+import waitForDockerDaemon from '../../helpers/dockerode/waitForDockerDaemon/waitForDockerDaemon';
+import isTransientDockerConnError from '../../helpers/dockerode/isTransientDockerConnError/isTransientDockerConnError';
+
+const dockerSafeCall = async <T>(
+  fn: (docker: Readonly<ReturnType<typeof getDocker>>) => Promise<T>,
+  totalTimeoutMs: number = 5000,
+  perAttemptTimeoutMs: number = 800,
+): Promise<T> => {
+  const logger = getLogger();
+
+  try {
+    return await fn(getDocker());
+  } catch (err) {
+    if (isTransientDockerConnError(err)) {
+      logger.warn({ err }, 'Docker pipe error, rebuilding client and retrying');
+
+      const docker = rebuildDocker();
+
+      await waitForDockerDaemon(docker, logger, {
+        totalTimeoutMs,
+        perAttemptTimeoutMs,
+      });
+
+      return fn(docker);
+    }
+
+    throw err;
+  }
+};
+
+export default dockerSafeCall;
