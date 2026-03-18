03654fe215add196f588cd2a1b0c837022abb76c
Daniele Angeli <daniele.angeli@molo17.com>
2026-03-18 10:34:40 +0100

    fix

--- packages/scripts/docker_images.sh
+++ packages/scripts/docker_images.sh
@@ -8,7 +8,7 @@ set -o pipefail
 # Default platform
 PLATFORM="linux/amd64,linux/arm64"
 
-FTP_BASE_DIR="/releases/linux"
+FTP_BASE_DIR="/releases/"
 FTP_TARGET_DIRS=()
 FTP_UPLOAD_ENABLED=false
 
@@ -39,7 +39,7 @@ map_release_dirs() {
 if [ -n "${RELEASE_TYPE:-}" ]; then
   if map_release_dirs "$RELEASE_TYPE" >/tmp/ftp_dirs.$$; then
     while IFS= read -r dir; do
-      FTP_TARGET_DIRS+=("$FTP_BASE_DIR/$dir")
+      FTP_TARGET_DIRS+=("$FTP_BASE_DIR/$dir/linux")
     done </tmp/ftp_dirs.$$
     rm -f /tmp/ftp_dirs.$$ >/dev/null 2>&1 || true
   else
@@ -99,7 +99,7 @@ upload_docker_tar() {
   rm -f "$tar_path"
 
   for target_dir in "${FTP_TARGET_DIRS[@]}"; do
-    local ftp_url="ftp://$FTP_SITE${target_dir}/linux/$remote_basename"
+    local ftp_url="ftp://$FTP_SITE${target_dir}/$remote_basename"
     echo "Uploading $(basename "$gz_path") to $ftp_url"
     curl --ftp-create-dirs -T "$gz_path" --user "$FTP_USER:$FTP_PASSWORD" "$ftp_url"
   done
