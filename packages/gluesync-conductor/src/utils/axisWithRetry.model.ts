16c1757b70ed48929eb8289f2e4fed767c632107
Alessandro Caregnato <alessandro.caregnato@molo17.com>
2026-04-08 17:37:40 +0200

    proxy for collect logs

--- packages/gluesync-conductor/src/utils/axisWithRetry.model.ts
+++ packages/gluesync-conductor/src/utils/axisWithRetry.model.ts
@@ -4,7 +4,14 @@ export type AxiosRetryOptions = Readonly<{
   backoffMs?: number;
 }>;
 
+export type AxiosRequestOptions = AxiosRetryOptions &
+  Readonly<{
+    method?: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'PROPFIND';
+    headers?: Record<string, string>;
+    data?: any;
+  }>;
+
 export type AxiosWithRetry = <T>(
   url: string,
-  options?: AxiosRetryOptions,
+  options?: AxiosRequestOptions,
 ) => Promise<T>;
