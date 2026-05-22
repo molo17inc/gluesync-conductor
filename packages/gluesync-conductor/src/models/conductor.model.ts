b3d55d464b0f932de5f583eef90a9de2549163dc
Alessandro Caregnato <alessandro.caregnato@molo17.com>
2026-05-22 17:05:08 +0200

    removed wrong piece of code

--- packages/gluesync-conductor/src/models/conductor.model.ts
+++ packages/gluesync-conductor/src/models/conductor.model.ts
@@ -15,20 +15,6 @@ export const releaseChannelTypes = ['alpha', 'beta', 'ga'] as const;
 
 export type ReleaseChannelTypes = (typeof releaseChannelTypes)[number];
 
-export const normalizeReleaseChannel = (
-  value?: string,
-): ReleaseChannelTypes | undefined => {
-  if (!value) {
-    return undefined;
-  }
-
-  const normalized = value.toLowerCase();
-
-  return releaseChannelTypes.includes(normalized as ReleaseChannelTypes)
-    ? (normalized as ReleaseChannelTypes)
-    : undefined;
-};
-
 export const containerActions = [
   'kill',
   'pull',
